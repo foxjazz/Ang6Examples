d5cdfb5433b194c8a99e4ffde73cdc448845c42b
mordagel <mordagel@outlook.com>
2018-07-03 22:37:49 -0600

    first commit2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  { path: 'first', component: FirstComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(routes, {enableTracing: true}),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -45,7 +45,7 @@ export class FirstComponent implements OnInit {
     try {
       this.data2 = Object.assign({}, this.data1);
     }
-    catch(e){
+    catch (e) {
       this.error = JSON.stringify(e);
     }
   }
